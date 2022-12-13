3f6db2e973b9d5b545a4b983e6a4f30d9a7b6a7d
NosherKhalid <nosherkhalid@gmail.com>
2022-12-13 21:08:28 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nosher Khalid on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
